be8b7fe405502d608db92501405ea10a1d53e1a5
Katarzyna Koloda <koloda.katarzyna@gmail.com>
2024-01-17 23:52:28 +0100

    added time series split visual

--- Appliances_energy_prediction/src/evaluation.py
+++ Appliances_energy_prediction/src/evaluation.py
@@ -26,16 +26,17 @@ def prediction_benchmarks(data, X_test, y_test):
     plt.plot(X_test.index, week_before, 
              label=f"week before: MAPE {mean_absolute_percentage_error(y_test, week_before):.2f}, "
                    f"MAE {mean_absolute_error(y_test, week_before):.0f}, "
-                   f"MSE {mean_squared_error(y_test, week_before):.0f}")
+                   f"RMSE {mean_squared_error(y_test, week_before, squared=False):.0f}")
     plt.plot(X_test.index, historical_means, 
              label=f"historical means: MAPE {mean_absolute_percentage_error(y_test, historical_means):.2f}, "
                    f"MAE {mean_absolute_error(y_test, historical_means):.0f}, "
-                   f"MSE {mean_squared_error(y_test, historical_means):.0f}")
+                   f"RMSE {mean_squared_error(y_test, historical_means, squared=False):.0f}")
     
     plt.gca().xaxis.set_major_locator(mdates.DayLocator())
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%a"))
     plt.legend()
     plt.title("Prediction benchmarks")
+    plt.tight_layout()
     plt.show()
 
 
@@ -63,21 +64,56 @@ def diagnose_errors(y_test, y_pred):
     plt.show()
 
 
-def evaluate_model(X_train, X_test, y_train, y_test, model, weights=None):
+def evaluate_model(X_train, X_test, y_train, y_test, model):
     
-    model.fit(X_train, y_train, model__sample_weight=weights)
+    model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
     mape = mean_absolute_percentage_error(y_test, y_pred)
-    mse = mean_squared_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred, squared=False)
     mae = mean_absolute_error(y_test, y_pred)
     
     plt.figure(figsize=(20,6))
     plt.plot(X_test.index, y_test)
     plt.plot(X_test.index, y_pred,
              color="red",
-             label=f"model prediction: MAPE {mape:.3f}, MSE {mse:.0f}, MAE {mae:.0f}")
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%A'))
+             label=f"model prediction: MAPE {mape:.3f}, RMSE {mse:.0f}, MAE {mae:.0f}")
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%a'))
     plt.legend()
     plt.show()
     print("\n\n")
     diagnose_errors(y_test, y_pred)
+
+
+def time_series_split(tscv, X_train):
+    
+    observation_ranges_train = []
+    observation_ranges_test = []
+    for train_index, test_index in tscv.split(X_train):
+        observation_ranges_train.append((train_index.min(), train_index.max() + 1))
+        observation_ranges_test.append((test_index.min(), test_index.max() + 1))
+    
+    plt.figure(figsize=(10, 4))
+    bar_width = 0.4
+    plt.barh(
+        range(len(observation_ranges_train)),
+        [end - start for start, end in observation_ranges_train],
+        left=[start for start, _ in observation_ranges_train],
+        label="Train"
+    )
+    plt.barh(
+        [idx for idx in range(len(observation_ranges_test))],
+        [end - start for start, end in observation_ranges_test],
+        left=[start for start, _ in observation_ranges_test],
+        label="Test"
+    )
+    plt.xlabel("Observation number", fontdict={"size":12})
+    plt.ylabel("Split", fontdict={"size":12})
+    plt.title("Observation ranges in TimeSeriesSplit")
+    plt.yticks(
+        [idx for idx in range(len(observation_ranges_train))],
+        [f"Split {i + 1}" for i in range(len(observation_ranges_train))]
+    )
+    plt.xticks(fontsize=12)
+    plt.yticks(fontsize=12)
+    plt.legend()
+    plt.show()
